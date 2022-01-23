1b424841cc2e456f17d57c699ca9ef89e10305f5
klm4life <mikophg2001@gmail.com>
2022-01-22 18:42:28 -0800

    finished core functionality for view details

--- frontend/src/components/Details.js
+++ frontend/src/components/Details.js
@@ -6,10 +6,20 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContentText from "@mui/material/DialogContentText";
 
-function Details({ expenses, details, toggleDetails }) {
+function Details({ details, toggleDetails, filteredExpenses }) {
+
   return (
-    <Dialog open={details} onClose={toggleDetails}>
+    <Dialog open={details} onClose={toggleDetails} >
       {/* display expenses here */}
+
+      {filteredExpenses.map(expense => {
+        return (
+          <div key={expense.id} className="info" style={{display: 'flex'}} >
+          <h1>{expense.data.name}</h1>
+          <h1>{expense.data.amount}</h1>
+          </div>
+        )
+      })}
     </Dialog>
   );
 }
