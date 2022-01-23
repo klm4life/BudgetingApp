ee31cb3d76142da6fa6ccc5a96b7a376c5cdbd97
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2022-01-23 07:08:29 +0700

    Refactor Stats component

--- frontend/src/components/Stats.js
+++ frontend/src/components/Stats.js
@@ -15,6 +15,30 @@ function Stats({ budgets, expenses }) {
   });
   const { totalBudgets, totalExpenses, budgetLeft } = stats;
 
+  const styles = {
+    boxContainer: {
+      borderRadius: "16px",
+      padding: "25px 0",
+      color: "#fff",
+      backgroundColor: "#1976d2",
+      textAlign: "center",
+      boxShadow: "rgb(99 99 99 / 20%) 0px 2px 8px 0px",
+    },
+    iconContainer: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "64px",
+      height: "64px",
+      borderRadius: "50%",
+      margin: "auto",
+      marginBottom: "24px",
+      color: "#fff",
+      backgroundImage:
+        "linear-gradient(135deg, rgba(12, 83, 183, 0) 0%, rgba(12, 83, 183, 0.24) 100%)",
+    },
+  };
+
   useEffect(() => {
     const calculateStats = () => {
       // get total budget
@@ -40,33 +64,9 @@ function Stats({ budgets, expenses }) {
     calculateStats();
   }, [budgets, expenses]);
 
-  const styles = {
-    boxContainer: {
-      boxShadow: "none",
-      textAlign: "center",
-      borderRadius: "16px",
-      padding: "25px 0",
-      color: "#fff",
-      backgroundColor: "#1976d2",
-    },
-    iconContainer: {
-      margin: "auto",
-      display: "flex",
-      borderRadius: "50%",
-      alignItems: "center",
-      width: "64px",
-      height: "64px",
-      justifyContent: "center",
-      marginBottom: "24px",
-      color: "#fff",
-      backgroundImage:
-        "linear-gradient(135deg, rgba(12, 83, 183, 0) 0%, rgba(12, 83, 183, 0.24) 100%)",
-    },
-  };
-
   return (
     <Grid container gap={2} sx={{ mt: "2em", mb: "5em" }}>
-      <Grid item xs={12} sm={4} md={4} sx={{ ...styles.boxContainer }}>
+      <Grid item xs={12} sm={4} sx={{ ...styles.boxContainer }}>
         <Box sx={{ ...styles.iconContainer }}>
           <TimelineIcon fontSize="large" />
         </Box>
@@ -75,7 +75,7 @@ function Stats({ budgets, expenses }) {
           Total Budget
         </Typography>
       </Grid>
-      <Grid item xs={12} sm={4} md={4} sx={{ ...styles.boxContainer }}>
+      <Grid item xs={12} sm={4} sx={{ ...styles.boxContainer }}>
         <Box sx={{ ...styles.iconContainer }}>
           <MoneyOffIcon fontSize="large" />
         </Box>
@@ -84,7 +84,7 @@ function Stats({ budgets, expenses }) {
           Total Expenses
         </Typography>
       </Grid>
-      <Grid item xs={12} sm={4} md={4} sx={{ ...styles.boxContainer }}>
+      <Grid item xs={12} sm={4} sx={{ ...styles.boxContainer }}>
         <Box sx={{ ...styles.iconContainer }}>
           <AttachMoneyIcon fontSize="large" />
         </Box>
